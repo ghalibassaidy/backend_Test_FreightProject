7b7d45c1fe87669ec1168ed2aeec6fa14dab089c
Ghalib Assaidy <ghalibassaidy55@gmail.com>
2025-08-14 11:05:20 +0700

    Configure for production deployment

--- core/settings.py
+++ core/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -145,3 +146,5 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [ 
     'http://localhost:5173',
     ]  
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
